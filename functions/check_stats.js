bb8503bab97cc1f6800d2d45138f50d431beddf8
Artem <artem@MacBook-Pro.local>
2026-02-22 20:06:45 +0100

    Cleanup: remove temporary debug scripts

--- functions/check_stats.js
+++
@@ -1,5 +0,0 @@
-import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebase-adminsdk.json"); // Assuming we have it or can use default credentials?
--- functions/run_backfill.ts
+++
@@ -1,5 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as fs from 'fs';
-
-// Since we have no service account directly, we can't easily script it if we aren't authorized.
-console.log("Not running");
